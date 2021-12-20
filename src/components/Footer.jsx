cb0e9bf6cf56c3fdbf067aa461b2b8d2a0a1451a
solutionprovider9174 <guangchengwang9174@yandex.com>
2021-12-20 15:16:30 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ import theme from '../theme';
 import {chakra, Box, Flex, SimpleGrid, GridItem, Heading, Text, Stack, FormControl, FormLabel,
     Input, InputGroup,  InputLeftAddon, FormHelperText, Textarea, Avatar, Icon, Button,  VisuallyHidden, Select, Checkbox,  RadioGroup, Radio, HStack, InputLeftElement, InputRightElement, Img
   } from "@chakra-ui/react";
-import { ButtonTransition, ImageTransition, InputTransition } from "./ImageTransition";
+import { ButtonBackTransition, ButtonTransition, ImageTransition, InputTransition } from "./ImageTransition";
 
 export default function Footer() {
     return (
         <footer
             className="container-fluid"
-            style={{ background: 'linear-gradient(90deg, rgba(54, 8, 71, 0.7) 0%, rgba(24, 7, 91, 0.7) 104.34%)' }}
+            style={{ background: 'linear-gradient(90deg, rgba(54, 8, 71, 0.7) 0%, rgba(24, 7, 91, 0.7) 104.34%)' , padding: '0px'}}
         >
             {/* <div className="social-share py-5">
                 <p className="mb-2">
@@ -46,36 +46,80 @@ export default function Footer() {
                 </a>
             </div> */}
             <ChakraProvider resetCSS theme={theme}>
-            <div style={{width:'100%', height:'100px'}}>
+            <div style={{width:'100%', height:'100px', borderBottom: '1.5px solid rgba(255, 255, 255, 0.1)'}}>
             <Flex mt='20px'>
                 <Text position='relative' height='34px' top='33px' left='99px' color='white' width='474px'>Wana Know more about WeFund?</Text>
                 <Input position='relative' style={{border: '1.5px solid rgba(255, 255, 255, 0.2)', top:'22px',
-                    bacground:'border: 1.5px solid rgba(255, 255, 255, 0.2)', borderRadius:'36px', 
+                    bacground:'border: 1.5px solid rgba(255, 255, 255, 0.2)', borderRadius:'36px', left:'300px'
                 }} type="text" h='50px' w='296px' rounded="md" placeholder='Enter email Adresses'  />
-                <Box position='relative' variant="solid" color="white" align='center' style={{
+                {/* <Box position='relative' variant="solid" color="white" align='center' style={{
                     background: 'linear-gradient(180deg, rgba(0, 193, 255, 0.1) 0%, rgba(0, 193, 255, 0.1) 100%)', borderRadius:'33px',
-                    top:'22px', border:'1px solid rgba(255, 255, 255, 0.05)'
+                    top:'22px', border:'1px solid rgba(255, 255, 255, 0.05)', left:'310px'
 
                 }} width= '133px' height='50px' top='33px'
                   onClick = {()=>{}} >
-                Subscribe
-              </Box>
+                <Text>Subscribe</Text>
+              </Box> */}
+              <div style={{display:'flex', position: 'relative', top:'22px', left:'310px'}}>
+                <ButtonBackTransition unitid="Subscribe"
+                selected={false}
+                width='133px' height='50px' rounded='33px' 
+                >
+                <Box variant="solid" color="white" justify='center' align='center'
+                    onClick = {()=>{document.location='/createproject'}} >
+                    Subscribe
+                </Box>
+                </ButtonBackTransition>
+            </div>
             </Flex>
             </div>
-            <div style={{width:'100%', height:'100px'}}>
-            <Flex justify='center'>
-                <Img width='56px' height='30px' position='relative' top='34px' src='/WeFund%20Logos%20only.png' alt='UST Avatar'/>
-                <Text mt='13px'>WeFund</Text>
-                
+            <div style={{width:'100%', height:'100px' ,borderBottom: '1.5px solid rgba(255, 255, 255, 0.1)'}}>
+            <Flex>
+                <Img width='56px' left='99px' height='30px' position='relative' top='34px' src='/WeFund%20Logos%20only.png' alt='UST Avatar'/>
+                <Text top ='32px' left='125px' height='30px' position='relative'  mt='13px' size='20px'>WeFund</Text>
+                <div>
+                    <Flex>
+                    <div key="about" className="btn-group" style={{cursor:'pointer', left:'540px'}}>
+                        <a href="" className="btn btn-danger "  
+                        style={{color:'rgba(255, 255, 255, 0.84)', height:'20px', top:'31px', fontSize:'15px', lineHeight:'18px'}}>
+                            About
+                        </a>
+                    </div>
+                    <div key="about" className="btn-group" style={{cursor:'pointer', left:'570px'}}>
+                        <a href="" className="btn btn-danger "  
+                        style={{color:'rgba(255, 255, 255, 0.84)', height:'20px', top:'31px', fontSize:'15px', lineHeight:'18px'}}>
+                            Contact
+                        </a>
+                    </div>
+                    <div key="about" className="btn-group" style={{cursor:'pointer', left:'600px'}}>
+                        <a href="" className="btn btn-danger "  
+                        style={{color:'rgba(255, 255, 255, 0.84)', height:'20px', top:'31px', fontSize:'15px', lineHeight:'18px'}}>
+                            Parnterships
+                        </a>
+                    </div>
+                    <div key="about" className="btn-group" style={{cursor:'pointer', left:'630px'}}>
+                        <a href="" className="btn btn-danger "  
+                        style={{color:'rgba(255, 255, 255, 0.84)', height:'20px', top:'31px', fontSize:'15px', lineHeight:'18px'}}>
+                            White Papers
+                        </a>
+                    </div>
+                    <div key="about" className="btn-group" style={{cursor:'pointer', left:'660px'}}>
+                        <a href="" className="btn btn-danger "  
+                        style={{color:'rgba(255, 255, 255, 0.84)', height:'20px', top:'31px', fontSize:'15px', lineHeight:'18px'}}>
+                            Terms of Service
+                        </a>
+                    </div>
+                    </Flex>
+                </div>
             </Flex>
             </div>
             <div style={{width:'100%', height:'65px'}}>
             <Flex>
-                <Text mt='13px'>@ 2021 WeFund. All rights reserved</Text>
-                <InstagramLogo size={31}/>
-                <TelegramLogo size={31}/>
-                <YoutubeLogo size={31}/>
-                <TwitterLogo size={31}/>
+                <Text mt='13px' position='relative' left='99px'>@ 2021 WeFund. All rights reserved</Text>
+                <InstagramLogo style={{position:'relative', left:'600px', top:'13px'}} size={31}/>
+                <TelegramLogo style={{position:'relative', left:'620px', top:'13px'}} size={31}/>
+                <YoutubeLogo style={{position:'relative', left:'640px', top:'13px'}} size={31}/>
+                <TwitterLogo style={{position:'relative', left:'660px', top:'13px'}} size={31}/>
             </Flex>
             </div>
             </ChakraProvider>
